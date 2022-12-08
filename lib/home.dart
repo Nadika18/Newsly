f98a959ffb958e1706aafc537b2f00f51092da12
Nadika Poudel <nadikapoudel16@gmail.com>
2022-12-08 16:12:47 +0545

    Card for news created

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
-import 'package:getwidget/getwidget.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'models/1.dart';
-// import 'dart:async' show Future;
+import 'dart:async' show Future;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -17,30 +16,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Newsly"),
-          
-          bottom: const TabBar(
-            tabs: [
-              Tab(text:"For You"),
-              Tab(text:"Popular"),
-              Tab(text:"Saved")
-            ]
+        appBar: AppBar(
+          title: const Text("Newsly"),
+          bottom: const TabBar(tabs: [
+            Tab(text: "For You"),
+            Tab(text: "Popular"),
+            Tab(text: "Saved")
+          ]),
+        ),
+        body: TabBarView(
+          children: [
+            ListView(
+              children: const <Widget>[
+                ElevatedCard(),
+                ElevatedCard(),
+                ElevatedCard(),
+                ElevatedCard(),
+              ],
             ),
-        ),
-        
-        body: TabBarView(children: [
-              ListView(
-          children: const <Widget>[
-            
-            ElevatedCard(),
-            ElevatedCard(),
-            ElevatedCard(),
-            ElevatedCard(),
+            const Text("Popular"),
+            const Text("Saved"),
           ],
-        ),
-        const Text("Popular"),
-        const Text("Saved"),
-        ],));
+        ));
   }
 }
 
@@ -55,20 +52,17 @@ class ElevatedCard extends StatefulWidget {
 //   return await rootBundle.loadString('assets/1.json');
 // }
 
-
-
 class _ElevatedCardState extends State<ElevatedCard> {
   // Future loadData() async {
   //   String jsonString = await _loadData();
   //   final jsonResponse = json.decode(jsonString);
-  //   News news = new News.fromJson(jsonResponse);
+  //   News news = News.fromJson(jsonResponse);
   //   print('${news.title}');
   // }
-  
+
   // void initState() {
   //   super.initState();
   //   loadData();
-    
   // }
 
   @override
@@ -76,22 +70,69 @@ class _ElevatedCardState extends State<ElevatedCard> {
   // final growableList= <NewsCategory>[]
 
   Widget build(BuildContext context) {
-    return GFCard(
-      boxFit: BoxFit.cover,
-      titlePosition: GFPosition.start,
-      image: Image.asset(
-        "assets/image1.png",
-        height: MediaQuery.of(context).size.height * 0.2,
-        width: MediaQuery.of(context).size.width,
-        fit: BoxFit.cover,
+    // return Container(
+    //   child:Column(children:[
+    //   Text('${news.title}'),
+    //   return GFCard(
+    //   boxFit: BoxFit.cover,
+    //   titlePosition: GFPosition.start,
+    //   image: Image.asset(
+    //     "assets/image1.png",
+    //     height: MediaQuery.of(context).size.height * 0.2,
+    //     width: MediaQuery.of(context).size.width,
+    //     fit: BoxFit.cover,
+    //   ),
+    //   showImage: true,
+    //   title: const GFListTile(
+    //     titleText: "text",
+    //     subTitleText: 'Author | Published date',
+    //   ),
+    //   content: const Text(
+    //       "Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card"),
+    // );
+    return Card(
+      
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            title: Text('The Enchanted Nightingale'),
+            subtitle: Text('Author | Published Date'),
+          ),
+         Container(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [ 
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image(image:AssetImage("assets/image1.png"))),
+
+             
+             SizedBox(height:20),
+              Text(
+                  "Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card"
+                )])),
+            
+            
+         
+     
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('SAVE'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
       ),
-      showImage: true,
-      title: const GFListTile(
-        titleText: "newstitle",
-        subTitleText: 'Author | Published date',
-      ),
-      content: const Text(
-          "Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card Some quick example text to build on the card"),
     );
   }
 }
--- lib/models/1.dart
+++ lib/models/1.dart
@@ -1,19 +1,20 @@
-// class News {
-//   String title;
-//   String description;
-//   String imagePath;
-//   String author;
+// ignore: file_names
+class News {
+  late String title;
+  late String description;
+  late String imagePath;
+  late String author;
 
-//   News(
-//       {required this.title,
-//       required this.description,
-//       required this.imagePath,
-//       required this.author});
-//   factory News.fromJson(Map<String, dynamic> data) {
-//     return News(
-//         title: data['title'],
-//         description: data['description'],
-//         imagePath: data['imagePath'],
-//         author: data['author']);
-//   }
-// }
+  News(
+      {required this.title,
+      required this.description,
+      required this.imagePath,
+      required this.author});
+  factory News.fromJson(Map<String, dynamic> data) {
+    return News(
+        title: data['title'],
+        description: data['description'],
+        imagePath: data['imagePath'],
+        author: data['author']);
+  }
+}
