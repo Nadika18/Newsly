01a2ade36b90d740cd5b2756a3e621ea9d927f95
Nadika Poudel <nadikapoudel16@gmail.com>
2022-12-10 19:52:42 +0545

    changes on model to parse json file

--- lib/models/1.dart
+++ lib/models/1.dart
@@ -1,24 +1,3 @@
-// // ignore: file_names
-// class News {
-//   late String title;
-//   late String description;
-//   late String imagePath;
-//   late String author;
-
-//   News(
-//       {required this.title,
-//       required this.description,
-//       required this.imagePath,
-//       required this.author});
-//   factory News.fromJson(Map<String, dynamic> data) {
-//     return News(
-//         title: data['title'],
-//         description: data['description'],
-//         imagePath: data['imagePath'],
-//         author: data['author']);
-//   }
-// }
-
 class News {
   late String title;
   late String description;
