845d9d20964d1b83cbad903a4c45c7a802d811e5
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-12-10 19:49:06 +0545

    Test commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 // ignore: depend_on_referenced_packages
@@ -125,31 +126,41 @@ class _ElevatedCardState extends State<ElevatedCard> {
                                   builder: (context) =>
                                       NewsDetailedView(news: news)));
                         },
-                        child: Card(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              ListTile(
-                                //return news.title
-                                title: Text(news.title),
-                                subtitle: Text(news.author),
-                              ),
-                              Container(
-                                  padding: const EdgeInsets.all(12),
-                                  child: Column(children: [
-                                    ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        child: AspectRatio(
-                                          aspectRatio: 16 / 9,
-                                          child: Image.network(news.imagePath,
-                                              fit: BoxFit.cover),
-                                        )),
-                                    const SizedBox(height: 20),
-                                    Text(
-                                        '${news.description.substring(0, 300)}...')
-                                  ])),
-                            ],
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(5, 15, 5, 15),
+                          child: Card(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                ListTile(
+                                  //return news.title
+                                  title: Text(
+                                    news.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  subtitle: Text(news.author),
+                                ),
+                                Container(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                    child: Column(children: [
+                                      ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          child: AspectRatio(
+                                            aspectRatio: 16 / 9,
+                                            child: Image.network(news.imagePath,
+                                                fit: BoxFit.cover),
+                                          )),
+                                      const SizedBox(height: 20),
+                                      Text(
+                                          '${news.description.substring(0, 300)}...')
+                                    ])),
+                              ],
+                            ),
                           ),
                         ),
                       );
@@ -172,10 +183,20 @@ class NewsDetailedView extends StatefulWidget {
 }
 
 class _NewsDetailedViewState extends State<NewsDetailedView> {
+  final player = AudioPlayer();
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('${widget.news.title}')),
-        body: Text(widget.news.author));
+        body: Container(
+            child: IconButton(
+                icon: Icon(Icons.speaker),
+                onPressed: () {
+                  player.play(widget.news.summary_tts);
+                })));
   }
 }
