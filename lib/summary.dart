f3e590c73a46f6a09e80241ec8c8bf69d9a52633
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-12-11 22:25:19 +0545

    Added navigation to summary cards

--- lib/summary.dart
+++ lib/summary.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'home.dart';
 import '/models/1.dart';
+import 'news_detailed.dart';
 
 class Summary extends StatefulWidget {
   const Summary({super.key});
@@ -39,46 +40,57 @@ class _SummaryState extends State<Summary> {
                       items: newsList?.toList().map((news) {
                         return Builder(
                           builder: (BuildContext context) {
-                            return Container(
-                                width: MediaQuery.of(context).size.width,
-                                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                                decoration: BoxDecoration(
-                                    color: Color.fromARGB(255, 255, 255, 255)),
-                                child: Card(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      ListTile(
-                                        //return news.title
-                                        title: Text(
-                                          '${news.title}',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18,
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            NewsDetailedView(news: news)));
+                              },
+                              child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                  decoration: BoxDecoration(
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                  child: Card(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        ListTile(
+                                          //return news.title
+                                          title: Text(
+                                            '${news.title}',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18,
+                                            ),
                                           ),
+                                          subtitle: Text('${news.author}'),
                                         ),
-                                        subtitle: Text('${news.author}'),
-                                      ),
-                                      Container(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              10, 0, 10, 0),
-                                          child: Column(children: [
-                                            ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(10.0),
-                                                child: AspectRatio(
-                                                  aspectRatio: 16 / 9,
-                                                  child: Image.network(
-                                                      '${news.imagePath}',
-                                                      fit: BoxFit.cover),
-                                                )),
-                                            const SizedBox(height: 20),
-                                            Text(
-                                                '${news.summary.characters.take(300)}...')
-                                          ])),
-                                    ],
-                                  ),
-                                ));
+                                        Container(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                10, 0, 10, 0),
+                                            child: Column(children: [
+                                              ClipRRect(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10.0),
+                                                  child: AspectRatio(
+                                                    aspectRatio: 16 / 9,
+                                                    child: Image.network(
+                                                        '${news.imagePath}',
+                                                        fit: BoxFit.cover),
+                                                  )),
+                                              const SizedBox(height: 20),
+                                              Text(
+                                                  '${news.summary.characters.take(300)}...')
+                                            ])),
+                                      ],
+                                    ),
+                                  )),
+                            );
                           },
                         );
                       }).toList(),
