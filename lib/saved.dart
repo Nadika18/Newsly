af1ee5f638c7e5b424f7441ce75c63834f3e42a6
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-12-13 19:40:37 +0545

    add saved

---
+++ lib/saved.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Saved extends StatefulWidget {
+  const Saved({super.key});
+
+  @override
+  State<Saved> createState() => _SavedState();
+}
+
+class _SavedState extends State<Saved> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 300, 0, 300),
+          child: Text('Newsly',
+              style: TextStyle(
+                fontFamily: 'Kalam',
+                fontSize: 35,
+                color: Colors.white,
+              )),
+        )),
+      ),
+    );
+  }
+}
